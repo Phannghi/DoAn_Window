d6cfbea44b910357ae9d5a499fdb955ac7062b52
Phannghi <phanphucnghi9a3thcspt2002@gmail.com>
2024-01-26 15:41:08 +0700

    bai tap git tuan 3

---
+++ baitaptuan03/baitaptuan03/Demo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitaptuan03
+{
+    internal class Demo
+    {
+        int x = 1;
+        int y = 2;
+        
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
